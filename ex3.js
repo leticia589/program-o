ecbf4e7439cfb76cbeebff25eeb81015bce22750
leticia <leticiacristinesn@gmail.com>
2022-08-09 22:41:16 -0300

    first commit

---
+++ ex3.js
@@ -0,0 +1,12 @@
+let nome = "Jose da silva";
+const idade = "27";
+const Peso = "83.5";
+const Altura ="1.70";
+const plano ="ouro"
+
+console.log(nome);
+console.log(idade);
+console.log(Peso);
+console.log(Altura);
+console.log(nome  +idade +ImageBitmapRenderingContext);
+
---
+++ mesa4.js
@@ -0,0 +1,5 @@
+let nome = "Marcos Santana";
+const cpf = "444.555.666.05";
+
+console.log(nome);
+console.log(cpf);
